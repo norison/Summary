788b325b098a2f63622e5c8d55fad302602dfa23
Ihor Matiev <ihor.matiev@gmail.com>
2021-12-08 18:13:29 +0200

    Added smooth scroll and up button, and renamed sass folder to scss

--- gulpfile.js
+++ gulpfile.js
@@ -18,7 +18,7 @@ gulp.task("server", function () {
 
 gulp.task("styles", function () {
   return gulp
-    .src("src/sass/**/*.+(scss|sass)")
+    .src("src/scss/**/*.scss")
     .pipe(sass({ outputStyle: "compressed" }).on("error", sass.logError))
     .pipe(rename({ suffix: ".min", prefix: "" }))
     .pipe(autoprefixer())
@@ -28,7 +28,7 @@ gulp.task("styles", function () {
 });
 
 gulp.task("watch", function () {
-  gulp.watch("src/sass/**/*.+(scss|sass)", gulp.parallel("styles"));
+  gulp.watch("src/scss/**/*.scss", gulp.parallel("styles"));
 });
 
 gulp.task("default", gulp.parallel("watch", "server", "styles"));
